1007b5b666e89955d2079138c0ed05eeb27ddda3
root <root@schuah42.schuah.42.fr>
2023-02-02 21:05:43 +0800

    Wordpress done

--- Inception/srcs/requirements/mariadb/scripts/mariadb_setup.sh
+++ Inception/srcs/requirements/mariadb/scripts/mariadb_setup.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
 
-mysql_install_db
-/etc/init.d/mysql start
-
-if [-d "/var/lib/mysql/$MYSQL_DATABASE"]
-then
+if [ -d /var/lib/mysql/$MYSQL_DATABASE ]; then
 	echo "Database exists"
 else
-	mysql_secure_installation << _EOF_
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql > /dev/null
 
-Y
-Chuahtseyung1
-Chuahtseyung1
-Y
-n
-Y
-Y
-_EOF_
-
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-mysql -uroot -p $MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
-
+	touch /usr/local/bin/init.sql
+	echo "USE mysql;
+		FLUSH PRIVILEGES;
+		CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+		GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+		GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+		FLUSH PRIVILEGES;" > /usr/local/bin/init.sql
+	mysqld --user=mysql --bootstrap < /usr/local/bin/init.sql
+#	/etc/init.d/mysql start
+#	mysql_secure_installation << _EOF_
+#
+#Y
+#Chuahtseyung1
+#Chuahtseyung1
+#Y
+#n
+#Y
+#Y
+#_EOF_
+#/etc/init.d/mysql stop
 fi
-
-/etc/init.d/mysql stop
 exec "$@"
