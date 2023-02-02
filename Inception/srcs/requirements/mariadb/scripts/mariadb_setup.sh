9da3914462d67788bd88949ed316282bf7c97234
root <root@schuah42.schuah.42.fr>
2023-02-02 23:53:02 +0800

    Mariadb side done

--- Inception/srcs/requirements/mariadb/scripts/mariadb_setup.sh
+++ Inception/srcs/requirements/mariadb/scripts/mariadb_setup.sh
@@ -11,19 +11,8 @@ else
 		CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 		GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
 		GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
 		FLUSH PRIVILEGES;" > /usr/local/bin/init.sql
 	mysqld --user=mysql --bootstrap < /usr/local/bin/init.sql
-#	/etc/init.d/mysql start
-#	mysql_secure_installation << _EOF_
-#
-#Y
-#Chuahtseyung1
-#Chuahtseyung1
-#Y
-#n
-#Y
-#Y
-#_EOF_
-#/etc/init.d/mysql stop
 fi
 exec "$@"
