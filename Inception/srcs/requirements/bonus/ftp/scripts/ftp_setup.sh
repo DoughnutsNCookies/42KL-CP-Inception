0c722116aa25e06c86d7b51f6a75fad1bae2d049
root <root@schuah42.schuah.42.fr>
2023-02-05 16:47:48 +0800

    FTP done

--- Inception/srcs/requirements/bonus/ftp/scripts/ftp_setup.sh
+++ Inception/srcs/requirements/bonus/ftp/scripts/ftp_setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
+if [ -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
+	echo "FTP has already been setup"
+else
 	mkdir -p /var/www/html
 	cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.bak
 	mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
 	adduser $FTP_USER --disabled-password
 	echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd &> /dev/null
 	chown -R $FTP_USER:$FTP_USER /var/www/html
-	echo $FTP_USER | tee -a /etc/vsftpd.userlist &> /dev/null
 fi
 
 /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
